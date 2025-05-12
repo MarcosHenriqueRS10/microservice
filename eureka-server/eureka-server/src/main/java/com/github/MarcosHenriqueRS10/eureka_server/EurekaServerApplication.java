ed0acae5a5107ac4bb63032357f0333a319d8af6
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-12 10:36:20 -0300

    first commit

---
+++ eureka-server/eureka-server/src/main/java/com/github/MarcosHenriqueRS10/eureka_server/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.MarcosHenriqueRS10.eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
